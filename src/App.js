da6208fddb5017186ed626e93dbf3a1b13e8b38d
NRnishad <nrnishad106@gmail.com>
2024-12-01 07:12:15 +0530

    cra&tailwind

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@
 import './App.css';
 
 function App() {
-  return <h1 className="text-3xl font-bold text-green-300">Hello world!</h1>;
+  return <h1 className="text-3xl font-bold text-red-600">Hello world!</h1>;
 }
 
 export default App;
